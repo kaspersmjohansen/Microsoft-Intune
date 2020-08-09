a7f494a9e48f16dea7968628ed0ac390e29708ef
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2020-08-09 17:58:08 +0200

    Create Install-Application.ps1
    Windows App (Win32) application wrapper

---
+++ App-Wrapper/Install-Application.ps1
@@ -0,0 +1,28 @@
+﻿# Script parameters
+Param(
+    [string]$Executable = "CitrixWorkspaceApp.exe",
+    [string]$InstallParameters = "/silent /noreboot /includeSSON ENABLE_SSON=YES",
+    [string]$UninstallParameters = "/uninstall /noreboot /silent",
+    [switch]$Install,
+    [switch]$Uninstall
+    )
+
+function Install-Application ($Executable, $InstallParameters, $UninstallParameters, $Install, $Uninstall)
+{
+# Get script dir
+$ScriptDir = (Get-Location).Path
+Push-Location $ScriptDir
+
+# Install application
+If ($Install)
+{
+    Start-Process -Wait $Executable -ArgumentList "$InstallParameters" -WindowStyle Minimized
+}
+        # Uninstall application
+        If ($Uninstall)
+        {
+            Start-Process -Wait $Executable -ArgumentList "$UninstallParameters" -WindowStyle Minimized
+        }
+}
+
+Install-Application $Executable $InstallParameters $UninstallParameters $Install $Uninstall
