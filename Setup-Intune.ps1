dbbb9adb973ab8bda7dce51122adfcacbc08112d
Kasper Johansen <kaspersmjohansen@gmail.com>
2022-10-25 05:40:46 +0200

    0.1 - Added code to create groups

--- Setup-Intune.ps1
+++ Setup-Intune.ps1
@@ -2,7 +2,10 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -Verbose
 
 # Install required Powershell Modules
 # Install-Module AzureAD -Repository PSGallery -Scope CurrentUser -Force
-Install-Module Microsoft.Graph.Intune -Repository -Scope CurrentUser -Force
+Install-Module Microsoft.Graph.Intune -Repository PSGallery -Scope CurrentUser -Force
+Install-Module Microsoft.Graph.Groups -Repository PSGallery -Scope CurrentUser -Force
+# Install-Module AzureAD -Repository PSGallery -Scope CurrentUser -Force
+
 
 # Function to create Intune groups
 function New-IntuneGroup
@@ -39,14 +42,41 @@ function New-IntuneGroup
 
 }
 
+# Connect-AzureAD -TenantID warlockstudy.net
+Connect-MSGraph
+Connect-AzureAD -TenantId warlockstudy.net
+
 # Get Intuneconfig.json contents
 $ConfigFilePath = "." + "\" + "IntuneConfig.json"
 $config = Get-Content -Path $ConfigFilePath -Raw | ConvertFrom-Json
 
-# Connect-AzureAD -TenantID warlockstudy.net
-get-help Connect-MSGraph
+$IntuneGroupName = $config.IntuneConfig.GroupName
+$IntuneGroupPrefix = $config.IntuneConfig.GroupPrefix
+$IntuneGroupAffix = $config.IntuneConfig.GroupAffix
+$IntuneAppGroup1 = $config.IntuneConfig.AppGroup1
+$IntuneAppGroup2 = $config.IntuneConfig.AppGroup2
+$IntuneAppGroup3 = $config.IntuneConfig.AppGroup3
+$IntuneAppGroup4 = $config.IntuneConfig.AppGroup4
+$IntuneAppGroup5 = $config.IntuneConfig.AppGroup5
 
-# Create PoC Group
-New-IntuneGroup -
-# Create App Groups
+# Create PoC group
+$IntunePoCGroup = New-IntuneGroup $IntuneGroupName -Prefix $IntuneGroupPrefix -Affix $IntuneGroupAffix
+
+# Create App groups
+$IntunePoCApp1 = New-IntuneGroup -Name $IntuneAppGroup1 -Prefix $IntuneGroupPrefix -Affix $IntuneGroupAffix
+$IntunePoCApp2 = New-IntuneGroup -Name $IntuneAppGroup2 -Prefix $IntuneGroupPrefix -Affix $IntuneGroupAffix
+$IntunePoCApp3 = New-IntuneGroup -Name $IntuneAppGroup3 -Prefix $IntuneGroupPrefix -Affix $IntuneGroupAffix
+$IntunePoCApp4 = New-IntuneGroup -Name $IntuneAppGroup4 -Prefix $IntuneGroupPrefix -Affix $IntuneGroupAffix
+$IntunePoCApp5 = New-IntuneGroup -Name $IntuneAppGroup5 -Prefix $IntuneGroupPrefix -Affix $IntuneGroupAffix
+
+# Add PoC group to App groups
+Add-AzureADGroupMember -ObjectId $IntunePoCApp1.groupid -RefObjectId $IntunePoCGroup.groupId
+Update-Groups -groupId $($IntunePoCGroup.groupId) -members $($IntunePoCApp1.groupid) -securityEnabled $true - #,$IntuneAppGroup2.groupid,$IntuneAppGroup3.groupid,$IntuneAppGroup4.groupid,$IntuneAppGroup5.groupid -securityEnabled $true
+
+Update-Groups -groupId "bc997475-95b9-41dc-8622-ed23ea6fa496" -memberOf "12557b25-0f9d-4bda-9e1e-1d73afe80c52"
+
+# Create Configuration Policies
+
+# Create Compliance Policies
+
 # Create Apps
