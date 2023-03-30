822b80f7cdbb4608f1c5a35996aa65eabf19d5ad
Kasper Johansen <kaspersmjohansen@gmail.com>
2023-03-30 12:03:13 +0200

    Initial commit

--- Set-OfficeTemplates.ps1
+++ Set-OfficeTemplates.ps1
@@ -1,8 +1,35 @@
-#-------------------------------------------------------------------------------- 
-# Name: virtualwarlock.net - Kasper Johansen
-# Set-OfficeTemplates.ps1, March 2023
-# 
-# ------------------------------------------------------------------------------------
+<#
+.NAME
+    Set-OfficeTemplates.ps1
+
+.SYNOPSIS
+    Copy Office (Word, Excel and Powerpoint) templates.
+
+.DESCRIPTION
+    To be able to use custom Office templates, the template files have to be copied to the Custom Office Templates folder in the user's Documents folder. 
+    This script copies .dotx, .xltx and .potx files to the Custom Office Templates folder.
+
+    Excel, Powerpoint and Word are then configured to allow Personal templates to be available when creating af new document. 
+    This configuration is delivered as registry changes for Excel, Powerpoint and Word in the HKCU:SOFTWARE\Microsoft\Office\16.0\ registry key
+
+    Lastly an Intune detection registry value is configured, to be able to detect if the script has run or not. 
+    This also enables versioning of the script using the $ScriptVersion variable
+
+    Variable documentation:
+
+    $CompanyName should contain the current companyname, or something unique to identify the company
+    $ScriptVersion should contain a version identifier such as "1.0", "1.1" etc.
+
+    $OfficeTemplatesPath contains the path of Custom Office Templates folder
+
+    A transcript logfile is created in :\Users\$env:username\$CompanyName which creates a verbose output if this script
+    
+.NOTES
+    Author:             Kasper Johansen
+    Website:            https://virtualwarlock.net
+    Last modified Date: 30-03-2022
+#>
+
 # Relaunch script in 64-bit context
 If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
     Try {
@@ -14,72 +41,90 @@ If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
     Exit
 }
 
-#Variables
-$CompanyName = "virtualwarlock.net"
+# Variables
+$CompanyName = ""
 $Scriptversion = "1.0"
-$OfficeTemplatesPath = "$env:APPDATA\Microsoft\Templates"
-$OutlookStationaryPath = "$env:APPDATA\Microsoft\Stationery"
+
+$OfficeTemplatesPath = "$($env:USERPROFILE)\Documents" + "\Custom Office Templates"
 
 # Configure log file and log file path
 $LogFile = "Set-OfficeTemplates.log"
-$LogPath = "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs"
+$LogPath = "$($env:USERPROFILE)\$CompanyName"
 
 If (!(Test-Path -Path $LogPath))
     {
         New-Item -Path $LogPath -ItemType Directory
     }
         
-    Start-Transcript -Path "$LogPath\$LogFile" | Out-Null
+    Start-Transcript -Path "$LogPath\$LogFile"
 
-# Create Templates folder, if not exist
+# Create Custom Office Templates folder, if not exist
 If (!(Test-Path -Path $OfficeTemplatesPath))
 {
     New-Item -Path $OfficeTemplatesPath -ItemType Directory -Verbose
 }
 
-# Copy Office templates
+# Copy Office templates to Custom Office Templates folder
 Get-ChildItem -Path .\Office -Include ("*.dotx","*.xltx","*.potx") -Recurse | Copy-Item -Destination $OfficeTemplatesPath -Force -Verbose
 
-# Create Stationery folder, if not exist
-If (!(Test-Path -Path $OutlookStationaryPath))
+# Create Excel, Powerpoint and Word Personal Templates Path registry value
+$OfficeApps = "Excel","Powerpoint","Word"
+ForEach ($App in $OfficeApps)
 {
-    New-Item -Path $OutlookStationaryPath -ItemType Directory -Verbose
+    $OfficeCurrentUserRegPath = "HKCU:SOFTWARE\Microsoft\Office\16.0"
+
+        If (!(Test-Path -Path "$OfficeCurrentUserRegPath\$App\Options"))
+        {
+            If (!(Test-Path -Path "HKCU:SOFTWARE\Microsoft\Office"))
+            {
+                New-Item -Path "HKCU:SOFTWARE\Microsoft" -Name "Office" -Verbose
+            }
+                If (!(Test-Path -Path "HKCU:SOFTWARE\Microsoft\Office\16.0"))
+                {
+                    New-Item -Path "HKCU:SOFTWARE\Microsoft\Office" -Name "16.0" -Verbose
+                }
+                    If (!(Test-Path -Path "HKCU:SOFTWARE\Microsoft\Office\16.0\$App"))
+                    {
+                        New-Item -Path "HKCU:SOFTWARE\Microsoft\Office\16.0" -Name "$App" -Verbose
+                    }
+                        If (!(Test-Path -Path "HKCU:SOFTWARE\Microsoft\Office\16.0\$App\Options"))
+                        {
+                            New-Item -Path "HKCU:SOFTWARE\Microsoft\Office\16.0\$App" -Name "Options" -Verbose
+                        }
+        }
+   
+    $PersonalTemplatesValue = (Get-ItemProperty "$OfficeCurrentUserRegPath\$App\Options" -ErrorAction SilentlyContinue).PersonalTemplates -eq $null 
+    If ($PersonalTemplatesValue -eq $False) {
+        Set-ItemProperty -Path "$OfficeCurrentUserRegPath\$App\Options" -Name "PersonalTemplates" -Value $OfficeTemplatesPath -Verbose
+    } Else {
+        New-ItemProperty -Path "$OfficeCurrentUserRegPath\$App\Options" -Name "PersonalTemplates" -Value $OfficeTemplatesPath -PropertyType ExpandString -Verbose
+    }
 }
 
-# Copy Outlook stationery
-Get-ChildItem -Path .\Outlook -Include ("*.thmx") -Recurse | Copy-Item -Destination $OutlookStationaryPath -Force -Verbose
-
-# Create registry key and value to detect if Office Templates are installed/copied
-$RegKey = "HKCU:Software\$CompanyName\Intune\OfficeTemplates"
+# Create Intune detection registry value
 $RegValue = "OfficeTemplates"
-$RegData = $Scriptversion      
+$RegKey = "HKCU:Software\$CompanyName\Intune\$RegValue"
+$RegData = $Scriptversion 
 
 If (!(Test-Path -Path $RegKey))
 {
-    New-Item -Path "HKCU:Software\$CompanyName" -Verbose
-    New-Item -Path "HKCU:Software\$CompanyName\Intune" -Verbose
-    New-Item -Path "HKCU:Software\$CompanyName\Intune\OfficeTemplates" -Verbose
-
-    New-ItemProperty -Path $RegKey -Name $RegValue -Value $RegData -PropertyType "String" -Verbose
-}
-else {
-    New-ItemProperty -Path $RegKey -Name $RegValue -Value $RegData -PropertyType "String" -Verbose
-}
-
-# Create registry key and value to detect if Outlook Stationery is installed/copied
-$RegKey = "HKCU:Software\$CompanyName\Intune\OfficeTemplates"
-$RegValue = "OutlookStationery"
-$RegData = $Scriptversion      
-
-If (!(Test-Path -Path $RegKey))
-{
-    New-Item -Path "HKCU:Software\$CompanyName" -Verbose
-    New-Item -Path "HKCU:Software\$CompanyName\Intune" -Verbose
-    New-Item -Path "HKCU:Software\$CompanyName\Intune\OfficeTemplates" -Verbose
-
-    New-ItemProperty -Path $RegKey -Name $RegValue -Value $RegData -PropertyType "String" -Verbose
-}
-else {
+    If (!(Test-Path -Path "HKCU:SOFTWARE\$CompanyName"))
+            {
+                New-Item -Path "HKCU:SOFTWARE" -Name "$CompanyName" -Verbose
+            }
+                If (!(Test-Path -Path "HKCU:SOFTWARE\$CompanyName\Intune"))
+                {
+                    New-Item -Path "HKCU:SOFTWARE\$CompanyName" -Name "Intune" -Verbose
+                }
+                    If (!(Test-Path -Path "HKCU:SOFTWARE\$CompanyName\Intune\$RegValue"))
+                    {
+                        New-Item -Path "HKCU:SOFTWARE\$CompanyName\Intune" -Name "$RegValue" -Verbose
+                    }
+}                     
+$RegValueExist = (Get-ItemProperty "$RegKey" -ErrorAction SilentlyContinue).$RegValue -eq $null 
+If ($RegValueExist -eq $False) {
+    Set-ItemProperty -Path $RegKey -Name $RegValue -Value $RegData -Verbose
+} Else {
     New-ItemProperty -Path $RegKey -Name $RegValue -Value $RegData -PropertyType "String" -Verbose
 }
 
