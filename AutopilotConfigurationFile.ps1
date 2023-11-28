1b3d0cddef189d489b17e14d6a0737e39e5b383f
Kasper Johansen <kaspersmjohansen@gmail.com>
2023-11-28 08:09:51 +0100

    Initial commit

--- AutopilotConfigurationFile.ps1
+++ AutopilotConfigurationFile.ps1
@@ -6,16 +6,16 @@ Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-PackageProvider -Name NuGet -Force
 
 # Import WindowsAutopilot Powershell module
-Install-Module AzureAD -Force -Scope CurrentUser
+# Install-Module AzureAD -Force -Scope CurrentUser
 Install-Module WindowsAutopilotIntune -Force -Scope CurrentUser
-Install-Module Microsoft.Graph.Intune -Force -Scope CurrentUser 
-Install-Module Microsoft.Graph.Authentication -Force -Scope CurrentUser
+# Install-Module Microsoft.Graph.Intune -Force -Scope CurrentUser 
+# Install-Module Microsoft.Graph.Authentication -Force -Scope CurrentUser
 Install-Module Microsoft.Graph.Identity.DirectoryManagement -Force -Scope CurrentUser
-Import-Module AzureAD
+# Import-Module AzureAD
 Import-Module WindowsAutopilotIntune
-Import-Module Microsoft.Graph.Intune
+# Import-Module Microsoft.Graph.Intune
 Import-Module Microsoft.Graph.Identity.DirectoryManagement
-Import-Module Microsoft.Graph.Authentication
+# Import-Module Microsoft.Graph.Authentication
 
 # Remember to have the correct permissions configured on the Graph Powershell or command-line app registration in Entra ID
 # Permissions are DeviceManagementConfiguration.Read.All,DeviceManagementManagedDevices.Read.All,DeviceManagementRBAC.Read.All,DeviceManagementServiceConfig.Read.All, Domain.Read.All
@@ -23,14 +23,48 @@ Import-Module Microsoft.Graph.Authentication
 Connect-MGGraph -Tenantid "pawsko.dk" -Scope DeviceManagementConfiguration.Read.All,DeviceManagementManagedDevices.Read.All,DeviceManagementRBAC.Read.All,DeviceManagementServiceConfig.Read.All, Domain.Read.All
 
 # Get Autopilot Configuration Profile
+# $AutoPilotConfigName = "AutopilotTEST"
+$AutoPilotConfigs = Get-AutoPilotProfile
+$AutoPilotJSONOutputFolder = "C:\Temp"
 
-$AutoPilotConfigName = "AutopilotTEST"
-$AutoPilotJSONOutputFolder = "C:\Users\KasperSvenMozartJoha\OneDrive - APENTO\Kunder\Paw Sko\AutoPilot Configs"
 If (!(Test-Path -Path $($AutoPilotJSONOutputFolder + "\" + $AutoPilotConfigName)))
 {
     New-Item -Path $AutoPilotJSONOutputFolder -Name $AutoPilotConfigName -ItemType Directory
 }
-Get-AutoPilotProfile -id $((Get-AutoPilotProfile | where {$_.DisplayName -eq $AutoPilotConfigName}).id)| ConvertTo-AutoPilotConfigurationJSON | Out-File -FilePath "$AutoPilotJSONOutputFolder\AutopilotConfigurationFile.json" -Encoding ASCII
+ForEach ($AutopilotConfig in $AutoPilotConfigs)
+{
+    $AutopilotProfileName = (Get-AutoPilotProfile -Id $AutopilotConfig.Id).DisplayName    
+    New-Item -Path $AutoPilotJSONOutputFolder -Name $AutopilotProfileName -ItemType Directory
+    Get-AutoPilotProfile -Id $AutopilotConfig.Id| ConvertTo-AutoPilotConfigurationJSON | Out-File -FilePath "$AutoPilotJSONOutputFolder\$AutopilotProfileName\AutopilotConfigurationFile.json" -Encoding ASCII
+}
 
-# Get the ZtdCorrelationID from the Auto pilot JSON file
-(device.enrollmentProfileName -eq "OfflineAutopilotprofile-0485ebb4-8bc6-4496-8c58-f36db48bd05e")
+# Create rule syntax with correlation ID from Autopilot profile JSON
+# (device.enrollmentProfileName -eq "OfflineAutopilotprofile-0485ebb4-8bc6-4496-8c58-f36db48bd05e")
+
+# Get correlation ID from Autopilot profile JSON
+$AutopilotConfigJSONs = Get-ChildItem -Path $AutoPilotJSONOutputFolder -Filter *.json -Recurse
+ForEach ($AutopilotConfigJSON in $AutopilotConfigJSONs)
+{
+    $Config = Get-Content -Path $AutopilotConfigJSON.Fullname -Raw | ConvertFrom-Json
+    #$Config.Comment_File
+    #$Config.ZtdCorrelationId
+    $Comment = $Config.Comment_File
+    $CorrelationID = $Config.ZtdCorrelationId
+    Write-Host "$Comment"
+    Write-host "(device.enrollmentProfileName -eq `"OfflineAutopilotprofile-$CorrelationID`")"
+    Write-Host ""
+}
+
+ForEach ($AutopilotConfig in $AutoPilotConfigs)
+{
+    $AutopilotProfileId = $AutopilotConfig.ID
+    Get-AutopilotProfileAssignments -id $AutopilotProfileId
+    #Get-AutoPilotProfile -id $AutopilotProfileId #| Get-AutopilotProfileAssignments
+}
+
+# Create Autopilot group
+$DisplayName = "Test Group"
+$Description = "Test Group"
+$MailNickName = "TestGroup"
+$MembershipRule = "(device.enrollmentProfileName -eq `"OfflineAutopilotprofile-0485ebb4-8bc6-4496-8c58-f36db48bd05e`")"
+New-MgGroup -DisplayName $DisplayName -Description $Description -MailEnabled:$False -MailNickname $MailNickName -SecurityEnabled:$True -GroupTypes "DynamicMembership" -MembershipRule $MembershipRule -MembershipRuleProcessingState "On"
