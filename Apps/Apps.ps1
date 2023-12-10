038dacfd5a09ac1cb9af718ad49dd6b21d9f5d57
Kasper Johansen <kaspersmjohansen@gmail.com>
2023-12-10 08:57:35 +0100

    Initial commit

--- Apps/Apps.ps1
+++ Apps/Apps.ps1
@@ -1,30 +1,42 @@
 $CSVFile = "$PSScriptRoot\AppsCSV.csv"
-$DownloadFolder = "$PSScriptRoot\Sources"
+$SourceFolder = "$PSScriptRoot\Sources"
+$DestinationFolder = "$PSScriptRoot"
 
 Import-Csv -Path $CSVFile -Delimiter ";" | ForEach-Object{
-    $AppName = $_.AppName
+    $Vendor = $_.Vendor
+    $Product = $_.Product
     $AppID = $_.AppID
-    $AppArchitecture = $_.AppArchitecture
+    $Architecture = $_.Architecture
     
-    $WinGetAppMetaExport = "$DownloadFolder\WinGetMetaExport-$AppName.txt"
+    $AppFolder = "$Vendor $Product $Architecture"
+    
+    $WinGetAppMetaExport = "$SourceFolder\WinGetMetaExport-$AppName.txt"
     Start-Process -FilePath "winget.exe" -ArgumentList "show --id $AppID --exact --accept-source-agreements" -WindowStyle Hidden -Wait -RedirectStandardOutput $WinGetAppMetaExport
     $winGetOutput = Get-Content -Path $WinGetAppMetaExport
     Remove-Item -Path $WinGetAppMetaExport -Force
 
     $Version = $winGetOutput | Select-String -Pattern "version:" | ForEach-Object { $_.Line -replace '.*version:\s*(.*)', '$1' }
 
-    If (!(Test-Path -Path "$DownloadFolder\$AppName"))
+    If (!(Test-Path -Path "$SourceFolder\$AppFolder"))
     {
-        New-Item -Path $DownloadFolder -name $AppName -ItemType Directory
+        New-Item -Path $SourceFolder -name $AppFolder -ItemType Directory
     }
 
-    If (!(Test-Path -Path "$DownloadFolder\$AppName\$Version"))
+    If (!(Test-Path -Path "$SourceFolder\$AppFolder\$Version"))
     {
-        New-Item -Path "$DownloadFolder\$AppName" -name $Version -ItemType Directory
-        Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --download-directory `"$DownloadFolder\$AppName\$Version`" --scope machine --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
+        New-Item -Path "$SourceFolder\$AppFolder" -name $Version -ItemType Directory
+        Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --download-directory `"$SourceFolder\$AppFolder\$Version`" --scope machine --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
     }
     else {
-        Write-Host "$AppName is already at latest version" -ForegroundColor Cyan
+        Write-Host "$AppFolder is already at latest version" -ForegroundColor Cyan
         Write-Host ""
+    }
+
+    If (!(Test-Path -Path "$DestinationFolder\$AppFolder"))
+    {
+        New-Item -Path $DestinationFolder -Name $AppFolder -ItemType Directory
+        New-Item -Path "$DestinationFolder\$AppFolder" -Name Media -ItemType Directory
+        Copy-Item -Path "$DestinationFolder\PSADT Template\*" -Destination "$DestinationFolder\$AppFolder\Media"
+        Copy-Item -Path "$DestinationFolder\DetectionScript.ps1" -Destination "$DestinationFolder\$AppFolder"
     }
 }
