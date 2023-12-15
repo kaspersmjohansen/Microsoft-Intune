ee6825274886b9edc0c1c04a3d6d46db269b17a3
Kasper Johansen <kaspersmjohansen@gmail.com>
2023-12-15 15:16:13 +0100

    App updated

--- Apps/Microsoft Hyper-V Management/Media/Deploy-Application.ps1
+++ Apps/Microsoft Hyper-V Management/Media/Deploy-Application.ps1
@@ -218,7 +218,42 @@ Try {
         [String]$installPhase = 'Post-Installation'
 
         ## <Perform Post-Installation tasks here>
+        # Hyper-V server hostnames
+        $HyperVServers = "srvhv01.johansen.local"
 
+        # Configure network profile to private
+        $NetworkProfile = Get-NetConnectionProfile
+        If ($NetworkProfile.NetworkCategory -eq "Public")
+        {
+            $EthernetIndex = (Get-NetConnectionProfile | where {$_.InterfaceAlias -eq "Ethernet"}).InterfaceIndex
+            Set-NetConnectionProfile -InterfaceIndex $EthernetIndex -NetworkCategory Private
+        }
+
+        # Enable PSRemoting
+        Enable-PSRemoting
+
+        # Configure Hyper-V Servers as trusted hosts
+        ForEach ($Server in $HyperVServers)
+        {
+            Set-Item WSMan:\localhost\Client\TrustedHosts -Value "$Server" -Force
+            Enable-WSManCredSSP -Role client -DelegateComputer "$Server" -Force
+        }
+
+        # Configure registry values to allow credential delegation
+        If (!(Test-Path -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation))
+        {
+            New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\" -Name 'CredentialsDelegation'
+        }
+
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\" -Name 'AllowFreshCredentialsWhenNTLMOnly'
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\" -Name 'AllowFreshCredentialsWhenNTLMOnly' -PropertyType DWord -Value "00000001"
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\" -Name 'ConcatenateDefaults_AllowFreshNTLMOnly' -PropertyType DWord -Value "00000001"
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\" -Name 'AllowFreshCredentials'
+
+        ForEach ($Server in $HyperVServers)
+        {
+            New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly\" -Name '1' -Value "wsman/$Server"
+        }
         ## Display a message at the end of the install
         #If (-not $useDefaultMsi) {
         #    Show-InstallationPrompt -Message 'You can customize text to appear at the end of an install or remove it completely for unattended installations.' -ButtonRightText 'OK' -Icon Information -NoWait
