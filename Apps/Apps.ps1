e1c2a656d1826e8685df05ff271e3f637be19396
Kasper Johansen <kaspersmjohansen@gmail.com>
2024-07-28 07:46:44 +0200

    Script updated to support a bit more automation

--- Apps/Apps.ps1
+++ Apps/Apps.ps1
@@ -1,6 +1,7 @@
 $CSVFile = "$PSScriptRoot\AppsCSV.csv"
 $SourceFolder = "$PSScriptRoot\Sources"
-$DestinationFolder = "$PSScriptRoot\_Production"
+$DestinationFolder = "$PSScriptRoot\_Dev"
+$ProductionAppFolder = "$PSScriptRoot\_Production"
 $PSADTTemplateFolder = "$PSScriptRoot\PSADT Template 3.10.1"
 
 Import-Csv -Path $CSVFile -Delimiter ";" | ForEach-Object {
@@ -10,8 +11,7 @@ Import-Csv -Path $CSVFile -Delimiter ";" | ForEach-Object {
     $Architecture = $_.Architecture
     $AppID = $_.AppID
     $Scope = $_.Scope
-    $AppFolder = "$Vendor $Product $Architecture"
-    
+    $SourceFileFolder = "$Vendor $Product $Architecture"  
     
     $WinGetAppMetaExport = "$SourceFolder\WinGetMetaExport-$AppID.txt"
     Start-Process -FilePath "winget.exe" -ArgumentList "show --id $AppID --exact --accept-source-agreements" -WindowStyle Hidden -Wait -RedirectStandardOutput $WinGetAppMetaExport
@@ -20,39 +20,52 @@ Import-Csv -Path $CSVFile -Delimiter ";" | ForEach-Object {
 
     $Version = $winGetOutput | Select-String -Pattern "version:" | ForEach-Object { $_.Line -replace '.*version:\s*(.*)', '$1' }
 
-    If (!(Test-Path -Path "$SourceFolder\$AppFolder"))
+    If (!(Test-Path -Path "$SourceFolder\$SourceFileFolder"))
     {
-        New-Item -Path $SourceFolder -name $AppFolder -ItemType Directory
+        New-Item -Path $SourceFolder -name $SourceFileFolder -ItemType Directory
     }
 
-    If (!(Test-Path -Path "$SourceFolder\$AppFolder\$Version"))
+    If (!(Test-Path -Path "$SourceFolder\$SourceFileFolder\$Version"))
     {
-        New-Item -Path "$SourceFolder\$AppFolder" -name $Version -ItemType Directory
+        New-Item -Path "$SourceFolder\$SourceFileFolder" -name $Version -ItemType Directory
         If ($Scope -eq "None")
         {
-            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$AppFolder\$Version`" --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
+            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$SourceFileFolder\$Version`" --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
         }
         elseif ($Scope -eq "Machine"){
-            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$AppFolder\$Version`" --scope $Scope --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
+            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$SourceFileFolder\$Version`" --scope $Scope --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
         }
         elseif ($Scope -eq "User"){
-            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$AppFolder\$Version`" --scope $Scope --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
+            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$SourceFileFolder\$Version`" --scope $Scope --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
         }
         else {
-            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$AppFolder\$Version`" --scope Machine --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
+            Start-Process -FilePath "winget.exe" -ArgumentList "download $AppID --exact --skip-dependencies --download-directory `"$SourceFolder\$SourceFileFolder\$Version`" --scope Machine --accept-package-agreements --accept-source-agreements" -Wait -NoNewWindow
         }
+    
+        $AppFolder = "$Vendor $Product $Architecture $Version"
+        $ProdApp = Get-ChildItem -Path $($ProductionAppFolder+"\"+$Vendor+" "+$Product+"*") | Select-Object -First 1
+        If (!(Test-Path -Path "$DestinationFolder\$AppFolder"))
+        {
+            New-Item -Path $DestinationFolder -Name $AppFolder -ItemType Directory
+            #New-Item -Path "$DestinationFolder\$AppFolder" -Name Media -ItemType Directory
+            
+            If ($ProdApp)
+            {
+                Copy-Item -Path "$ProdApp\*" -Recurse -Destination "$DestinationFolder\$AppFolder" -Force
+                Get-ChildItem -Path "$SourceFolder\$SourceFileFolder" | Where-Object {$_.extension -in ".exe",".msi"} | Copy-Item -Destination "$DestinationFolder\$AppFolder\Media\Files" -Force
+            }
+            else
+            {
+                Copy-Item -Path "$PSADTTemplateFolder\*" -Recurse -Destination "$DestinationFolder\$AppFolder"
+                #Copy-Item -Path "$DestinationFolder\DetectionScript.ps1" -Destination "$DestinationFolder\$AppFolder"
+                Get-ChildItem -Path "$SourceFolder\$SourceFileFolder" | Where-Object {$_.extension -in ".exe",".msi"} | Copy-Item -Destination "$DestinationFolder\$AppFolder\Media\Files"
+            }
+        }
+
     }
     else {
-        Write-Host "$AppFolder is already at latest version" -ForegroundColor Cyan
+        Write-Host "$SourceFileFolder is already at latest version" -ForegroundColor Cyan
         Write-Host ""
     }
 
-    If (!(Test-Path -Path "$DestinationFolder\$AppFolder"))
-    {
-        New-Item -Path $DestinationFolder -Name $AppFolder -ItemType Directory
-        #New-Item -Path "$DestinationFolder\$AppFolder" -Name Media -ItemType Directory
-        Copy-Item -Path "$PSADTTemplateFolder\*" -Recurse -Destination "$DestinationFolder\$AppFolder"
-        #Copy-Item -Path "$DestinationFolder\DetectionScript.ps1" -Destination "$DestinationFolder\$AppFolder"
-        Get-ChildItem -Path "$SourceFolder\$AppFolder\$Version" | Where-Object {$_.extension -in ".exe",".msi"} | Copy-Item -Destination "$DestinationFolder\$AppFolder\Media\Files"
-    }
 }
