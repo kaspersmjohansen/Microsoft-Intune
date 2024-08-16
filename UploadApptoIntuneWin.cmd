423ff03a96f99d1efca8516837fb20fae5508582
Kasper Johansen <kaspersmjohansen@gmail.com>
2024-08-16 12:05:14 +0200

    Added for increased automation

---
+++ UploadApptoIntuneWin.cmd
@@ -0,0 +1,3 @@
+cls
+powershell.exe -executionpolicy bypass -file UploadApptoIntuneWin.ps1 -SourcePath "C:\Users\WDAGUtilityAccount\Desktop\_Build" -OutputAppFolder c:\temp -TenantID virtualwarlock.net -IntuneAppName Short -AppRegistrationConfigPath C:\Users\WDAGUtilityAccount\Desktop\Microsoft-Intune
+pause
