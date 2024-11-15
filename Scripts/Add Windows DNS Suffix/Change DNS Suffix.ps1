1e12bb2b7886b5ff6edb64ef2a8fcf6627d37d9b
Kasper Johansen <kaspersmjohansen@gmail.com>
2024-11-15 12:06:33 +0100

    Moved to another repo

--- Scripts/Add Windows DNS Suffix/Change DNS Suffix.ps1
+++
@@ -1,3 +0,0 @@
-$DNSSuffixDomain = "johansen.local"
-$ConnectionSuffix = (Get-DnsClient | Where-Object -Property InterfaceAlias -Match Ethernet).ConnectionSpecificSuffix
-Set-DnsClientGlobalSetting -SuffixSearchList @("$ConnectionSuffix","$DNSSuffixDomain")
