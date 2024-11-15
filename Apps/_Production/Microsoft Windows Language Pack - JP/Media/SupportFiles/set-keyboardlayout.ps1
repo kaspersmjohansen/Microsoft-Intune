9fc8b940e7630bf67791c8892b03b89df8e3c927
Kasper Johansen <kaspersmjohansen@gmail.com>
2024-11-15 12:26:53 +0100

    Moved to another repo

--- Apps/_Production/Microsoft Windows Language Pack - JP/Media/SupportFiles/set-keyboardlayout.ps1
+++
@@ -1,9 +0,0 @@
-# Script for Scheduled task
-$LanguageID = "ja-JP"
-$OldList = Get-WinUserLanguageList
-$NewUserLanguage = New-WinUserLanguageList -Language $LanguageID
-$InputMethodTips = $NewUserLanguage.InputMethodTips
-$UserLanguageList= $($OldList | where { $_.LanguageTag -ne $LanguageID }) + $NewUserLanguage
-$UserLanguageList | select LanguageTag
-Set-WinUserLanguageList -LanguageList $UserLanguageList -Force
-Set-WinDefaultInputMethodOverride -InputTip $InputMethodTips
--- Apps/_Production/Microsoft Windows Language Pack/Media/SupportFiles/set-keyboardlayout.ps1
+++
@@ -1,9 +0,0 @@
-# Script for Scheduled task
-$LanguageID = "da-DK"
-$OldList = Get-WinUserLanguageList
-$NewUserLanguage = New-WinUserLanguageList -Language $LanguageID
-$InputMethodTips = $NewUserLanguage.InputMethodTips
-$UserLanguageList = $($OldList | Where-Object { $_.LanguageTag -ne $LanguageID }) + $NewUserLanguage
-$UserLanguageList | select LanguageTag
-Set-WinUserLanguageList -LanguageList $UserLanguageList -Force
-Set-WinDefaultInputMethodOverride -InputTip $InputMethodTips
